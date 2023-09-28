86840862a2a7fcc315cd9295d95704cd1b337944
Catalina <acata06@gmail.com>
2023-09-27 22:44:23 -0300

    Noveno cambio

---
+++ script.js
@@ -0,0 +1,7 @@
+DarkButton = document.querySelector("input")
+
+function cambiarImagen(){
+    document.body.classList.toggle("Darkk_Button")
+    document.body.classList.toggle("hola")
+}
+DarkButton.addEventListener("click",cambiarImagen)
