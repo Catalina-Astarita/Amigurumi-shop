87359a84ec7763c5940bf5c701123e703be8c06d
Catalina <acata06@gmail.com>
2023-10-02 23:08:24 -0300

    Doceavo cambio

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-DarkButton = document.querySelector("input")
+const lis = document.querySelectorAll(".list-group-item")
+const buscador = document.querySelector("#buscador")
 
-function cambiarImagen(){
-    document.body.classList.toggle("Darkk_Button")
-    document.body.classList.toggle("hola")
+
+for (i=0;i<lis.length;i+2){
+    lis[i].classList.add("list-group-item-secondary")
 }
-DarkButton.addEventListener("click",cambiarImagen)
+
+
+
+//Funcion 1
+function filtrar(substring, elements){
+   cumplen = [] 
+for (i of lis){
+    if(i.innerText.toLowerCase().includes(substring.toLowerCase())){
+       cumplen.push(i)
+   }
+ }
+ return cumplen
+}
+
+//Funcion 2
+buscador.addEventListener("input", ()=>{
+    if (buscador.value == " "){
+        for (i of lis){
+            i.classList.remove("d-none")
+        }
+    }else{
+        filtrados = filtrar(buscador.value,lis)
+        for(i of lis){
+            i.classList.add("d-none")
+        }
+        for (i of filtrados){
+            i.classList.remove("d-none")
+        }
+    }
+})
