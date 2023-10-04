a48aed7ae1918f17c29c241f605b0799803c5fca
Catalina <acata06@gmail.com>
2023-10-03 23:06:18 -0300

    Treceavo cambio

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-const lis = document.querySelectorAll(".list-group-item")
+const lis = document.querySelectorAll(".fin")
 const buscador = document.querySelector("#buscador")
-
-
-for (i=0;i<lis.length;i+2){
-    lis[i].classList.add("list-group-item-secondary")
-}
-
 
 
 //Funcion 1
 function filtrar(substring, elements){
    cumplen = [] 
-for (i of lis){
+for (i of elements){
     if(i.innerText.toLowerCase().includes(substring.toLowerCase())){
        cumplen.push(i)
    }
@@ -19,14 +13,26 @@ for (i of lis){
  return cumplen
 }
 
+//function strip_ul(){
+//shown_list = document.querySelectorAll("li:not(.d-none)") 
+//for (i of shown_lis){
+//    i.classList.remove("list-group-item-secondary")
+//} 
+//for (i=0;i<shown_lis.length;i+=2){
+ //   shown_lis[i].classList.add("list-group-item-secondary")
+//}
+//}
+
+
 //Funcion 2
 buscador.addEventListener("input", ()=>{
-    if (buscador.value == " "){
+    console.log("carlitos")
+    if (buscador.value == ""){
         for (i of lis){
             i.classList.remove("d-none")
         }
     }else{
-        filtrados = filtrar(buscador.value,lis)
+        filtrados = filtrar(buscador.value, lis)
         for(i of lis){
             i.classList.add("d-none")
         }
@@ -34,4 +40,7 @@ buscador.addEventListener("input", ()=>{
             i.classList.remove("d-none")
         }
     }
+    strip_ul()
 })
+
+strip_ul()
